ffa4acdd8269561bc11f2f47d3a058c2d81a1bad
Kereminhoo <kerem.icel@condorcet.be>
2026-09-21 10:13:32 +0200

    exercices

---
+++ src/main/java/be/condorcet/demo1/SecondeServlet.java
@@ -0,0 +1,49 @@
+package be.condorcet.demo1;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "SecondeServelet", value = "/SecondeServelet")
+public class SecondeServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+
+        String nom = request.getParameter("nom");
+        String age = request.getParameter("age");
+
+        out.println("<html><body>");
+        out.println("<h1>Bienvenue</h1>");
+
+        if (nom != null && age != null && !age.isEmpty()) {
+            try {
+
+                int age1 = Integer.parseInt(age);
+                out.print("<p>Bonjour " + nom + " | ");
+
+                if (age1 >= 18) {
+                    out.print("tu peux passer ton permis !</p>");
+                } else {
+                    out.print("J'espère que tu as ton skateboard.</p>");
+                }
+            } catch (NumberFormatException e) {
+                out.println("<p>Erreur : l'age doit etre un nombre valide.</p>");
+            }
+        } else {
+            out.println("<p>Veuillez fournir un nom et un âge valides dans le lien.</p>");
+        }
+
+        out.println("</body></html>");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,25 @@
+<%@ page contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>JSP - Hello World</title>
+</head>
+<body>
+<h1><%= "Hello World!" %></h1>
+<br/>
+<a href="hello-servlet">Hello Servlet</a>
+<br/><br/>
+
+<hr>
+<h3>Exercice API 4 : Test de la SecondeServelet</h3>
+
+
+<p>
+    <a href="SecondeServelet?nom=Kerem&age=21">Test avec Kerem, 21 ans (> 18)</a>
+</p>
+<p>
+    <a href="SecondeServelet?nom=Samed&age=15">Test avec Samed, 15 ans (< 18)</a>
+</p>
+
+</body>
+</html>
